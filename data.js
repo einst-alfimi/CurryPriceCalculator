750941f4c0116b8dbb55f4be9655debed3ddec51
einst-alfimi <einst.4lchemie@gmail.com>
2019-03-13 13:12:35 +0900

    create toppings
    腹減ってきた

---
+++ data.js
@@ -0,0 +1,48 @@
+const toppings = [
+    {name:'クリーミータルタルソース', price:51},
+    {name:'ガーリック', price:51},
+    {name:'半熟タマゴ', price:72},
+    {name:'ゆでタマゴ', price:72},
+    {name:'オクラ山芋', price:103},
+    {name:'単品ポテト', price:103},
+    {name:'完熟カットトマト', price:103},
+    {name:'赤たまねぎのピクルス', price:81},
+    {name:'赤たまタルタルソース', price:105},
+    {name:'半熟タマゴタルタルソース', price:123},
+
+    {name:'ツナ', price:103},
+    {name:'コーン', price:103},
+
+    {name:'なす', price:154},
+    {name:'納豆', price:154},
+    {name:'フィッシュフライ', price:154},
+    {name:'スクランブルエッグ', price:154},
+
+    {name:'たっぷりあさり', price:185},
+    {name:'エビあさり', price:195},
+    {name:'チーズ', price:217},
+
+    {name:'イカ', price:206},
+    {name:'クリームコロッケ(カニ入り)', price:206},
+    {name:'エビにこみ', price:206},
+    {name:'きのこ', price:206},
+    {name:'トマトアスパラ', price:206},
+
+    {name:'やさい', price:226},
+    {name:'ほうれん草', price:226},
+
+    {name:'ロースカツ', price:290},
+    {name:'チキンカツ', price:290},
+    {name:'パリパリチキン', price:290},
+    {name:'ソーセージ', price:290},
+
+    {name:'海の幸', price:299},
+
+    {name:'チーズインハンバーグ', price:308},
+    {name:'ビーフカツ', price:360},
+    {name:'エビカツ', price:432},
+    {name:'手仕込ささみカツ', price:456},
+    {name:'手仕込ヒレ・ささみカツ', price:460},
+    {name:'手仕込ヒレカツ', price:463},
+    {name:'手仕込とんかつ', price:463},
+]
