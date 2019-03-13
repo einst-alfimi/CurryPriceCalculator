952f84e6d7a8fc6f5493d6165a045a7ce60df957
einst-alfimi <einst.4lchemie@gmail.com>
2019-03-13 18:49:16 +0900

    失われしトッピングを追加
    漏れです

--- data.js
+++ data.js
@@ -9,35 +9,32 @@ const toppings = [
     {name:'赤たまねぎのピクルス', price:81},
     {name:'赤たまタルタルソース', price:105},
     {name:'半熟タマゴタルタルソース', price:123},
-
     {name:'ツナ', price:103},
     {name:'コーン', price:103},
-
     {name:'なす', price:154},
     {name:'納豆', price:154},
     {name:'フィッシュフライ', price:154},
     {name:'スクランブルエッグ', price:154},
-
     {name:'たっぷりあさり', price:185},
     {name:'エビあさり', price:195},
     {name:'チーズ', price:217},
-
     {name:'イカ', price:206},
     {name:'クリームコロッケ(カニ入り)', price:206},
     {name:'エビにこみ', price:206},
     {name:'きのこ', price:206},
     {name:'トマトアスパラ', price:206},
-
     {name:'やさい', price:226},
     {name:'ほうれん草', price:226},
-
+    {name:'チキンにこみ', price:238},
+    {name:'フライドチキン', price:238},
+    {name:'ハンバーグ', price:258},
+    {name:'豚しゃぶ', price:258},
+    {name:'メンチカツ', price:258},
     {name:'ロースカツ', price:290},
     {name:'チキンカツ', price:290},
     {name:'パリパリチキン', price:290},
     {name:'ソーセージ', price:290},
-
     {name:'海の幸', price:299},
-
     {name:'チーズインハンバーグ', price:308},
     {name:'ビーフカツ', price:360},
     {name:'エビカツ', price:432},
